ef0eb792d18af158a847e71f3a1befef28840aa8
Himanshu Keshri <thehimanshukeshri@gmail.com>
2018-02-09 22:11:24 +0530

    Day 1 : 09-02-18

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Baby Brilliant High School</title>
+  </head>
+  <body>
+    <h1> Baby Brilliant High School</h1>
+  </body>
+</html>
---
+++ public/staff/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css"/>
+    <title>BBHS-STAFF AREA</title>
+  </head>
+  <body>
+    <header>
+      <h1> BBHS- STAFF AREA</h1>
+    </header>
+
+    <navigation>
+      <ul>
+          <li><a href="index.php">Menu </a></li>
+      </ul>
+    </navigation>
+
+    <div id="content">
+
+    </div>
+
+    <footer>
+      &copy;<?php echo date('Y'); ?> BBHS,Patna
+    </footer>
+
+
+  </body>
+</html>
