faab6cb3f1561ba5e1500cc8c5d39f9a5f61d981
Himanshu Keshri <thehimanshukeshri@gmail.com>
2018-04-09 00:56:17 +0530

    Navigation and page content

--- private/shared/public_navigation.php
+++ private/shared/public_navigation.php
@@ -1,23 +1,38 @@
+<?php
+if(isset($page_id)){
+  $page_id = $page_id;
+} else{
+  $page_id = '';
+}
+if(isset($subject_id)){
+  $subject_id = $subject_id;
+} else{
+  $subject_id = '';
+}
+?>
+
+
 <navigation>
   <?php $nav_subjects=find_all_subjects(); ?>
   <ul class="subjects">
     <?php while ($nav_subject=mysqli_fetch_assoc($nav_subjects)){ ?>
-      <li>
-        <a href="<?php echo url_for('index.php'); ?>">
+      <li class="<?php if($nav_subject['id'] == $subject_id){ echo "selected";} ?>">
+        <a href="<?php echo url_for('index.php?subject_id='.h(u($nav_subject['id']))); ?>">
           <?php echo h($nav_subject['menu_name']); ?>
         </a>
 
+        <?php if($nav_subject['id'] ==  $subject_id) { ?>
         <?php $nav_pages=find_pages_by_subject_id($nav_subject['id']); ?>
         <ul class="pages">
           <?php while ($nav_page=mysqli_fetch_assoc($nav_pages)){ ?>
-            <li>
-              <a href="<?php echo url_for('index.php'); ?>">
+            <li class="<?php if($nav_page['id'] == $page_id){ echo "selected";} ?>">
+              <a href="<?php echo url_for('index.php?id='. h(u($nav_page['id']))); ?>">
                 <?php echo h($nav_page['menu_name']); ?>
               </a>
             </li>
           <?php } //end of while nav_page?>
         </ul>
-
+      <?php } ?>
       </li>
     <?php } //end of while nav_subject?>
   </ul>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,28 @@
 <?php require_once('../private/intialize.php'); ?>
 
+<?php
+if(isset($_GET['id'])){
+  $page_id=$_GET['id'];
+  $page=find_page_by_id($page_id);
+  if(!$page){
+  redirect_to(url_for('/index.php'));
+  }
+  $subject_id = $page['subject_id'];
+}
+elseif(isset($_GET['subject_id'])){
+  $subject_id = $_GET['subject_id'];
+  $page_set = find_pages_by_subject_id($subject_id);
+  $page = mysqli_fetch_assoc($page_set);
+  if(!$page){
+    redirect_to(url_for('/index.php'));
+  }
+  $page_id=$page['id'];
+}
+else{
+  //nothing to do
+}
+ ?>
+
 <?php include(SHARED_PATH.'/public_header.php'); ?>
 
 <div class="" id="main">
@@ -8,9 +31,12 @@
   <div class="" id="page">
 
     <?php
-    include(SHARED_PATH.'/static_homepage.php');
+    if(isset($page)){
+      echo $page['content'];
+    }else{
+      include(SHARED_PATH.'/static_homepage.php');
+    }
     ?>
-himanshu
   </div>
 
 </div>
